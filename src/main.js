4b0cff528670e90209f7df9aa7f2c1f24a531d20
vcardct <vanessa.cardenas@digitalcomtech.com>
2023-07-18 14:41:38 -0500

    Llamar las categoriías y mostrarlas en lista en UI

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// -------------- MOSTRAR TRENDING MOVIES EN EL INICIO ------------------
 async function getTrendingMoviesPreview(){
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json();
@@ -19,4 +20,29 @@ async function getTrendingMoviesPreview(){
     });
 }
 
+// ------------------------ LISTA DE CATEGORÍAS  -----------------------------
+
+async function getCategoriesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres;
+    categories.forEach(category => {
+        const getCategoriesPreviewContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3'); // Esto se encuentra en el index.html
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id); //    concatenar para que le salgan los colorcitos a las categorías
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        getCategoriesPreviewContainer.appendChild(categoryContainer);
+    });
+}
+
 getTrendingMoviesPreview();
+getCategoriesPreview();
